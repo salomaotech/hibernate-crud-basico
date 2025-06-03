d22dca195967ab61ed7f764daf852a591b6faeff
salomaotech <salomao@taimber.com>
2025-06-03 14:26:12 -0300

    Adicionado métodos de persistência: remover, getbyid, update

--- src/main/java/com/salomaotech/Main.java
+++ src/main/java/com/salomaotech/Main.java
@@ -1,6 +1,7 @@
 package com.salomaotech;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.salomaotech.models.Cliente;
 import com.salomaotech.repositories.ClienteRepository;
@@ -16,7 +17,17 @@ public class Main {
         Cliente cliente = new Cliente();
         cliente.setNome("João Silva");
         cliente.setTelefone("99999-9999");
-        clienteRepository.save(cliente);
+        Optional<Cliente> c = clienteRepository.save(cliente);
+
+        if (c.isPresent()) {
+
+            System.out.println("Cadastro bem sucedido!");
+
+        } else {
+
+            System.out.println("Cadastro mal sucedido!");
+
+        }
 
     }
 
@@ -34,10 +45,58 @@ public class Main {
 
     }
 
+    private static void remover(long id) {
+
+        if (clienteRepository.remove(id)) {
+
+            System.out.println("Registro removido!!!");
+
+        } else {
+
+            System.out.println("Registro não removido!!!");
+
+        }
+
+    }
+
+    private static void getById(long id) {
+
+        Optional<Cliente> cliente1 = clienteRepository.getById(id);
+
+        if (cliente1.isPresent()) {
+
+            System.out.println(cliente1.get().getNome());
+
+        }
+
+    }
+
+    private static void update() {
+
+        Cliente cliente = new Cliente();
+        cliente.setId(6);
+        cliente.setNome("Salomão F Silva");
+        cliente.setTelefone("62 99999-9999");
+
+        if (clienteRepository.update(cliente).isPresent()) {
+
+            System.out.println("Cadastro atualizado!");
+
+        } else {
+
+            System.out.println("Cadastro não atualizado!");
+
+        }
+
+    }
+
     public static void main(String[] args) {
 
         save();
         findAll();
+        remover(3);
+        getById(7);
+        update();
         jpaUtil.close();
         System.exit(0);
 
--- src/main/java/com/salomaotech/repositories/ClienteRepository.java
+++ src/main/java/com/salomaotech/repositories/ClienteRepository.java
@@ -1,6 +1,8 @@
 package com.salomaotech.repositories;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.salomaotech.models.Cliente;
 import com.salomaotech.utils.JpaUtil;
 
@@ -16,32 +18,110 @@ public class ClienteRepository {
         this.jpaUtil = jpaUtil;
     }
 
-    public Cliente save(Cliente cliente) {
+    public Optional<Cliente> save(Cliente cliente) {
 
         // Prepara transação
         EntityManager manager = jpaUtil.entityManager();
         EntityTransaction transaction = manager.getTransaction();
         transaction.begin();
 
-        // Persiste
-        manager.persist(cliente);
-        transaction.commit();
-        manager.close();
+        try {
 
-        return cliente;
+            // Persiste
+            manager.persist(cliente);
+            transaction.commit();
+            manager.close();
+            return Optional.of(cliente);
+
+        } catch (Exception ex) {
+
+            manager.close();
+            return null;
+
+        }
 
     }
 
     public List<Cliente> findAll() {
 
+        // Prepara a pesquisa via JPQL
         EntityManager manager = jpaUtil.entityManager();
-
         TypedQuery<Cliente> query = manager.createQuery("select obj from " + Cliente.class.getSimpleName() + " obj",
                 Cliente.class);
 
+        // Carrega e retorna os resultados
         List<Cliente> clientes = query.getResultList();
         manager.close();
         return clientes;
+
+    }
+
+    public Optional<Cliente> getById(long id) {
+
+        // Prepara transação
+        EntityManager manager = jpaUtil.entityManager();
+
+        try {
+
+            Cliente cliente = manager.find(Cliente.class, id);
+            manager.close();
+            return Optional.of(cliente);
+
+        } catch (Exception ex) {
+
+            manager.close();
+            return Optional.empty();
+
+        }
+
+    }
+
+    public boolean remove(long id) {
+
+        // Prepara transação
+        EntityManager manager = jpaUtil.entityManager();
+        EntityTransaction transaction = manager.getTransaction();
+        transaction.begin();
+
+        // Carrega o registro
+        Cliente cliente = manager.find(Cliente.class, id);
+
+        try {
+
+            manager.remove(cliente);
+            transaction.commit();
+            manager.close();
+            return true;
+
+        } catch (Exception ex) {
+
+            manager.close();
+            return false;
+
+        }
+
+    }
+
+    public Optional<Cliente> update(Cliente cliente) {
+
+        // Prepara transação
+        EntityManager manager = jpaUtil.entityManager();
+        EntityTransaction transaction = manager.getTransaction();
+        transaction.begin();
+
+        try {
+
+            // Atualiza
+            cliente = manager.merge(cliente);
+            transaction.commit();
+            manager.close();
+            return Optional.of(cliente);
+
+        } catch (Exception ex) {
+
+            return Optional.empty();
+
+        }
 
     }
 
